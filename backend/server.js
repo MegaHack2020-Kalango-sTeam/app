15ccc5c2d1af62160e2670cd26673d11ac85efaa
Jhony Terra <jhony.wt@gmail.com>
2020-05-01 01:27:15 -0300

    Criando o servidor e importando as rotas.

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+//Importando  express pra auto atualização.
+const express = require('express');
+//Importando mongoose pra mongoDB.
+const mongoose = require('mongoose');
+//Importando as rotas do app.
+const routes = require ('/routes');
+
+//Colocando nosso APP para utilizar o Express.
+const app = express();
+//Tornando o app responsivo ao .json.
+app.use(express.json());
+//Indicando em qual porta irá rodar nosso servidor.
+app.listen(3333);
